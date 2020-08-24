e6c0c67120cfb1ba9b188afb4ad5ba3955a0ad62
Sammy Tran <sammytran277@gmail.com>
2020-08-24 16:49:57 -0400

    Change affiliated stores and improve the positioning at different viewports

--- src/components/AffiliateStores.js
+++ src/components/AffiliateStores.js
@@ -2,16 +2,16 @@ import React from "react";
 import "../stylesheets/AffiliateStores.css";
 
 // Affiliated stores
-import nike from "../img/affiliate-images/nike.png";
-import flowers from "../img/affiliate-images/flowers.png";
-import colehaan from "../img/affiliate-images/colehaan.png";
-import overstock from "../img/affiliate-images/overstock.png";
-import microsoft from "../img/affiliate-images/microsoft.png";
-import expedia from "../img/affiliate-images/expedia.png";
+import foot_locker from "../img/affiliate-images/foot_locker.png";
+import freshly from "../img/affiliate-images/freshly.png";
 import glossier from "../img/affiliate-images/glossier.png";
-import bloomingdale from "../img/affiliate-images/bloomingdale.png";
-import samsung from "../img/affiliate-images/samsung.png";
-import jcpenney from "../img/affiliate-images/jcpenney.png";
+import casper from "../img/affiliate-images/casper.png";
+import kohls from "../img/affiliate-images/kohls.png";
+import levis from "../img/affiliate-images/levis.png";
+import nixon from "../img/affiliate-images/nixon.png";
+import petco from "../img/affiliate-images/petco.png";
+import ulta_beauty from "../img/affiliate-images/ulta_beauty.png";
+import vans from "../img/affiliate-images/vans.png";
 
 const AffiliateStores = () => {
   return (
@@ -19,16 +19,16 @@ const AffiliateStores = () => {
       <h1 id="affiliate-title">
         Shop Sustainably at <br></br>10,000+ stores!
       </h1>
-      <img className="store-lg" id="nike" src={nike} alt="nike logo"></img>
-      <img className="store-md" id="flowers" src={flowers} alt="flowers logo"></img>
-      <img className="store-md" id="colehaan" src={colehaan} alt="colehaan logo"></img>
-      <img className="store-lg" id="overstock" src={overstock} alt="overstock logo"></img>
-      <img className="store-md" id="microsoft" src={microsoft} alt="microsoft logo"></img>
-      <img className="store-lg" id="expedia" src={expedia} alt="expedia logo"></img>
+      <img className="store-lg" id="foot-locker" src={foot_locker} alt="foot-locker logo"></img>
+      <img className="store-md" id="freshly" src={freshly} alt="freshly logo"></img>
       <img className="store-md" id="glossier" src={glossier} alt="glossier logo"></img>
-      <img className="store-md" id="bloomingdale" src={bloomingdale} alt="bloomingdale logo"></img>
-      <img className="store-lg" id="samsung" src={samsung} alt="samsung logo"></img>
-      <img className="store-md" id="jcpenney" src={jcpenney} alt="jcpenney logo"></img>
+      <img className="store-lg" id="casper" src={casper} alt="casper logo"></img>
+      <img className="store-md" id="kohls" src={kohls} alt="kohls logo"></img>
+      <img className="store-lg" id="levis" src={levis} alt="levis logo"></img>
+      <img className="store-md" id="nixon" src={nixon} alt="nixon logo"></img>
+      <img className="store-md" id="petco" src={petco} alt="petco logo"></img>
+      <img className="store-lg" id="ulta_beauty" src={ulta_beauty} alt="ulta_beauty logo"></img>
+      <img className="store-md" id="vans" src={vans} alt="vans logo"></img>
     </section>
   );
 };
